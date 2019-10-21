1c2a6be3b6e5399d4cb6a20d600dcaaae0a925da
Diego Dutra <Diego.Dutra@processor.com.br>
2019-10-21 16:19:49 -0300

    Ajuste para abrir direto API direto na url do Swagger

--- WebApiUser/Startup.cs
+++ WebApiUser/Startup.cs
@@ -49,6 +49,7 @@ namespace WebApiUser
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
             
             if (env.IsDevelopment())
